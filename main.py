603c6cd482bd4b677d8ee0dcd559a54b7a978e40
ANGELMC113 <108123779+ANGELMC113@users.noreply.github.com>
2024-05-02 11:33:21 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+# Llegeix les entrades, pregunta què fer i crida les funcions adequades.
+
+''' Llibreries que hauríem de fer servir:
+
+requests per baixar-vos fitxers de dades.
+gpxpy per llegir fitxers GPX.
+beatifulsoup per llegir arbres HTML.
+simplekml per escriure fitxers KML.
+networkx per a manipular grafs.
+scikit per calcular agrupaments.
+haversine per a calcular distàncies entre coordenades.
+staticmap per pintar mapes.
+
+'''
