4b18309ada283d4b4b72462280fa7865fc422331
Nuriadg <168724516+Nuriadg@users.noreply.github.com>
2024-05-28 12:56:44 +0200

    Update graphmaker.py

--- Rutes_i_monuments_Angel_M_Nuria_D2/graphmaker.py
+++ Rutes_i_monuments_Angel_M_Nuria_D2/graphmaker.py
@@ -42,15 +42,43 @@ def make_graph(centroid_labels: "tuple[ndarray[int]", centroid_coords: list[Poin
     return G
 
 
-def simplify_graph(G: Graph, epsilon: float) -> Graph:
+
+
+def _calculate_angle(p1: Point,p2: Point,p3: Point)-> float:
+    '''Returns an angle in degrees given a point and its neighbors'''
+    """
+    c1 = np.array([p1.lat, p1.lon]) #node
+    c2 = np.array([p2.lat, p2.lon]) # nbor1
+    c3 = np.array([p3.lat, p3.lon]) # nbor2
+    """
+    v1 = [p2.lat - p1.lat , p2.lon -p1.lon] 
+    v2 = [p3.lat - p1.lat, p3.lon - p1.lon]
+
+    #a = np.linalg.norm(v1)
+    #b = np.linalg.norm(v2)
+    prod_escalar = v1[0]*v2[0] + v1[1]*v2[1]
+
+    #angle = np.arccos((np.cross(v1,v2))/(a*b)) # [0, pi]
+    angle = math.acos(prod_escalar/(math.dist(p1,p2)*math.dist(p1,p3)))
+
+    return angle*180/math.pi
+
+
+def simplify_graph(G: nx.Graph, epsilon: float) -> nx.Graph:
     """Simplify the graph."""
-    # si un node té més de 2 arestes, no n'en pot perdre. Per tant, només ens cal analitzar cada node un cop.
+    nodes_to_remove = set()
     for node in G.nodes:
-        if len(G.neighbors(node)) == 2:
-            # Try to erase node
+        neighbors = G.adj[node]
+        if len(neighbors) == 2: # If a node more than three nodes we can't lose it. We only have to analize once each node then.
             # If angle less than epsilon: erase node
-            # Angle is computed: 
-            ...
+            angle = _calculate_angle(G.nodes[node['point']], G.nodes[neighbors[0]]['point'], G.nodes[neighbors[1]]['point'])
+
+            if angle >= 180 - epsilon:
+                nodes_to_remove.add(node)
+    
+    G.remove_nodes_from(nodes_to_remove)
+    return G
+
 
 
 def draw_graph(G: Graph):
