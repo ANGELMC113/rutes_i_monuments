0a8e1391f32c702164725e7e78efeb435f191876
ANGELMC113 <108123779+ANGELMC113@users.noreply.github.com>
2024-05-31 22:57:26 +0200

    Delete viewer.py

--- viewer.py
+++
@@ -1,9 +0,0 @@
-
-
-def export_PNG(graph: nx.Graph, filename: str) -> None:
-    """Export the graph to a PNG file using staticmap."""
-
-
-def export_KML(graph: nx.Graph, filename: str) -> None:
-    """Export the graph to a KML file."""
-
