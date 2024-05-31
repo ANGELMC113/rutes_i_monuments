a822717e4b3aebe79791a468dc239e44a4d830fb
ANGELMC113 <108123779+ANGELMC113@users.noreply.github.com>
2024-05-31 22:58:00 +0200

    Delete graphmaker.py

--- graphmaker.py
+++
@@ -1,7 +0,0 @@
-import networkx as nx
-
-def make_graph(segments: Segments, clusters: int) -> nx.Graph:
-    """Make a graph from the segments."""
-
-def simplify_graph(graph: nx.Graph, epsilon: float) -> nx.Graph:
-    """Simplify the graph."""
